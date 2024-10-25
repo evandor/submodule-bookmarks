f08af8ede68c127e1fc11a7eccc3b33b31298c40
Carsten Gräf <evandor@gmail.com>
2024-10-25 14:12:30 +0200

    merge from tabsets

--- models/TreeNodeInfo.ts
+++ models/TreeNodeInfo.ts
@@ -2,7 +2,7 @@ import {TreeNode} from "src/bookmarks/models/Tree";
 
 export class TreeNodeInfo {
   constructor(
-    public treeNode?:TreeNode,
+    public treeNode:TreeNode | undefined = undefined,
     public folderCount: number,
     public leafCount: number ) {
   }
